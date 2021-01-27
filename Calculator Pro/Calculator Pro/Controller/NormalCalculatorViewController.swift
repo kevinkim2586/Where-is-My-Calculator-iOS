e6c7cf8c27fbc1a6ad821b7fae61bc170305c811
김영채 <kevinkim2586@gmail.com>
2021-01-27 19:11:31 +0900

    연속 계산 구현
    기존에는 1+2 = 3 이런 식으로 2개의 값만 계산 가능했으나 이제는 1+2+3+4+5 = result 가 될 수 있도록 구현 완료

--- Calculator Pro/Calculator Pro/Controller/NormalCalculatorViewController.swift
+++ Calculator Pro/Calculator Pro/Controller/NormalCalculatorViewController.swift
@@ -9,37 +9,18 @@ class NormalCalculatorViewController: UIViewController {
     var workings: String = ""
     
     var numPressedFirst: Bool = false
+    var continuingInput: Bool = false
+    
     var first: Double = 0.0
     var second: Double = 0.0
     var op: String = ""
     var result: Double = 0.0
     
-    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
-    //MARK: - Functions
-    
-    // Clears all two labels
-    func clearAll(){
-        workings = ""
-        calculatorWorkings.text = ""
-        calculatorResults.text = "0"
-    }
-    
-    // Only clears the result label
-    func clearResultsLabel(){
-        
-        workings = ""
-        calculatorResults.text = ""
-    }
-    
-    func addToWorkings(value: String){
-        
-        workings = workings + value
-        calculatorWorkings.text = workings
-    }
+   
     
     //MARK: - Numbers
     
@@ -56,10 +37,25 @@ class NormalCalculatorViewController: UIViewController {
         op = sender.currentTitle!
         
         if(numPressedFirst){
-            first = Double(workings) ?? 0
-            calculatorWorkings.text = workings + sender.currentTitle!
-            clearResultsLabel()
-            numPressedFirst = false
+            
+            if(continuingInput == true){
+                
+                calculatorWorkings.text = String(format: "%.0f", first + Double(workings)!) + op
+                
+                first = first + Double(workings)!
+                
+                clearResultsLabel()
+            }
+            else{
+                calculatorWorkings.text = workings + sender.currentTitle!
+                first = Double(workings) ?? 0
+                
+                clearResultsLabel()
+                numPressedFirst = false
+                
+                continuingInput = true
+            }
+            
         }
         else{
             return
@@ -94,17 +90,15 @@ class NormalCalculatorViewController: UIViewController {
         case "/": result = first / second
             
         default:
-            print("Error in displaying number")
+            print("Error in calculate()")
         }
     }
     
     
     
+
     
     
-    
-    
-    //MARK: - Calculator Functions
     @IBAction func allClearTap(_ sender: UIButton) {
         clearAll()
     }
@@ -116,9 +110,19 @@ class NormalCalculatorViewController: UIViewController {
             calculatorResults.text = workings
         }
     }
+    // Clears all two labels
+    func clearAll(){
+        workings = ""
+        calculatorWorkings.text = ""
+        calculatorResults.text = "0"
+    }
     
-    
-       
+    // Only clears the result label
+    func clearResultsLabel(){
+        
+        workings = ""
+        calculatorResults.text = ""
+    }
     
     
     
