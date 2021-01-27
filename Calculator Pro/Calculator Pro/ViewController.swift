b8f4f5cec2d13087d438182287ab6992da3a71ab
김영채 <kevinkim2586@gmail.com>
2021-01-27 14:12:09 +0900

    각 버튼 IBOutlet 추가

--- Calculator Pro/Calculator Pro/ViewController.swift
+++ Calculator Pro/Calculator Pro/ViewController.swift
@@ -8,12 +8,90 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var calculatorWorkings: UILabel!
+    @IBOutlet weak var calculatorResults: UILabel!
+    
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    
+    //MARK: - Functions
+    @IBAction func allClearTap(_ sender: UIButton) {
+    }
+    
+    @IBAction func backTap(_ sender: UIButton) {
+    }
+    
+    @IBAction func percentTap(_ sender: UIButton) {
+    }
+    
+    @IBAction func divideTap(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func multiplyTap(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func minusTap(_ sender: UIButton) {
+    }
+    
+    @IBAction func addTap(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func equalTap(_ sender: UIButton) {
+    }
+    
+    
+    
+    //MARK: - Numbers
+    
+    @IBAction func decimalTap(_ sender: UIButton) {
+    }
+    
+    @IBAction func zeroTap(_ sender: UIButton) {
+    }
+    
+    @IBAction func oneTap(_ sender: UIButton) {
+    }
+    @IBAction func twoTap(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func threeTap(_ sender: UIButton) {
+    }
+    
+    @IBAction func fourTap(_ sender: UIButton) {
+    }
+    
+    @IBAction func fiveTap(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func sixTap(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func sevenTap(_ sender: UIButton) {
+    }
+    
+    @IBAction func eightTap(_ sender: UIButton) {
+    }
+    
+    @IBAction func nineTap(_ sender: UIButton) {
+    }
+    
+    
+    
+    
+    
 }
 
