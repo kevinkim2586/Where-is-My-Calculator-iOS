ff6c447a22ed92943530b723f55430689d7b5fb4
김영채 <kevinkim2586@gmail.com>
2021-01-27 15:36:29 +0900

    HomeViewController 와 NormalCalculatorViewController 연결

--- Calculator Pro/Calculator Pro/Controller/HomeViewController.swift
+++ Calculator Pro/Calculator Pro/Controller/HomeViewController.swift
@@ -1,18 +1,21 @@
-//
-//  HomeViewController.swift
-//  Calculator Pro
-//
-//  Created by Kevin Kim on 2021/01/27.
-//
-
 import UIKit
+
 
 class HomeViewController: UIViewController {
 
+    @IBOutlet weak var normalCalculatorButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        
+    }
+    
+    
+    @IBAction func pressedNormalCalculatorButton(_ sender: UIButton) {
+        
+        
     }
     
 
