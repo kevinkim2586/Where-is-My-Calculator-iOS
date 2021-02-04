8547ab6f01ce6eda0c33d5da38283c412c418f63
김영채 <kevinkim2586@gmail.com>
2021-02-04 14:56:37 +0900

    Gold VC - PickerView 구현

--- Calculator Pro/Calculator Pro/Controller/GoldCalculatorViewController.swift
+++ Calculator Pro/Calculator Pro/Controller/GoldCalculatorViewController.swift
@@ -4,7 +4,7 @@ import UIKit
 class GoldCalculatorViewController: UIViewController {
 
     
-    @IBOutlet weak var goldUnitPicker: UITextField!
+    @IBOutlet weak var goldUnitPickerTextField: UITextField!
     @IBOutlet weak var userInputTextField: UITextField!
     @IBOutlet weak var resultTextField: UITextField!
     
@@ -22,6 +22,8 @@ class GoldCalculatorViewController: UIViewController {
         userInputTextField.inputView = UIView()
         userInputTextField.becomeFirstResponder()
         
+        createPickerView()
+    
         
         
     }
@@ -105,15 +107,59 @@ extension GoldCalculatorViewController: UITextFieldDelegate{
 
 //MARK: - Picker View Related Methods
 
-extension GoldCalculatorViewController{
+extension GoldCalculatorViewController: UIPickerViewDataSource, UIPickerViewDelegate{
+    
     
     func createPickerView(){
         
         let goldUnitPickerView = UIPickerView()
+        goldUnitPickerView.backgroundColor = .white
         goldUnitPickerView.dataSource = self
         goldUnitPickerView.delegate = self
         
-        goldUnitPicker
+        let toolBar = UIToolbar()
+        toolBar.barStyle = UIBarStyle.default
+        toolBar.isTranslucent = true
+        toolBar.tintColor = UIColor(red: 76/255, green: 217/255, blue: 100/255, alpha: 1)
+        toolBar.sizeToFit()
+
+        let doneButton = UIBarButtonItem(title: "Done", style: UIBarButtonItem.Style.done, target: self, action: #selector(self.dismissPicker))
+        let spaceButton = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.flexibleSpace, target: nil, action: nil)
+        let cancelButton = UIBarButtonItem(title: "Cancel", style: UIBarButtonItem.Style.plain, target: self, action: #selector(self.dismissPicker))
+
+        toolBar.setItems([cancelButton, spaceButton, doneButton], animated: false)
+        toolBar.isUserInteractionEnabled = true
+        
+        
+        goldUnitPickerTextField.inputView = goldUnitPickerView
+        goldUnitPickerTextField.inputAccessoryView = toolBar
+        
+        
     }
+    
+    @objc func dismissPicker(){
+        self.view.endEditing(true)
+    }
+    
+    
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return goldCalculatorManager.goldUnitArray.count
+    }
+    
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return goldCalculatorManager.goldUnitArray[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        
+        goldUnitPickerTextField.text = goldCalculatorManager.goldUnitArray[row]
+    }
+    
     
 }
