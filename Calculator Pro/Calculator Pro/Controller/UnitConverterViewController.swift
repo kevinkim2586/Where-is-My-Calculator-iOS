e362c3e163872780ec24070a421c195b8ff2531b
김영채 <kevinkim2586@gmail.com>
2021-02-16 12:31:14 +0900

    Add basic functionality for popup view

--- Calculator Pro/Calculator Pro/Controller/UnitConverterViewController.swift
+++ Calculator Pro/Calculator Pro/Controller/UnitConverterViewController.swift
@@ -2,13 +2,56 @@ import UIKit
 
 class UnitConverterViewController: UIViewController {
 
+    @IBOutlet weak var unitFromButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
        
     }
     
+    
+    @IBAction func showUnitSelectionListButton(_ sender: UIButton) {
+        
+        let button = sender as UIButton
+        let buttonFrame = button.frame
+        
+        let popoverContentController = storyboard?.instantiateViewController(withIdentifier: Constants.StoryboardID.unitPopoverStoryboardID) as? UnitPopOverContentController
+        
+        popoverContentController?.modalPresentationStyle = .popover
+        
+        if let popoverPresentationController = popoverContentController?.popoverPresentationController{
+            
+            popoverPresentationController.permittedArrowDirections = .up
+            popoverPresentationController.sourceView = self.view
+            popoverPresentationController.sourceRect = buttonFrame
+            popoverPresentationController.delegate = self
+            
+            if let popoverController = popoverContentController{
+                present(popoverController, animated: true, completion: nil)
+            }
+        }
+        
+    }
 
+}
 
-
+extension UnitConverterViewController: UIPopoverPresentationControllerDelegate{
+    
+    
+    func adaptivePresentationStyle(for controller: UIPresentationController) -> UIModalPresentationStyle {
+        return .none
+    }
+    
+//    func presentationControllerDidDismiss(_ presentationController: UIPresentationController) {
+//        <#code#>
+//    }
+    
+    func presentationControllerShouldDismiss(_ presentationController: UIPresentationController) -> Bool {
+        return true
+    }
+    
+    
+    
 }
---
+++ Calculator Pro/Calculator Pro/Controller/UnitPopOverContentController.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+class UnitPopOverContentController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+     
+    }
+    
+
+
+
+}
