e5dc9f2a493f47859e72862b169ba7602e6ca508
김영채 <kevinkim2586@gmail.com>
2021-02-18 13:59:33 +0900

    fix enum operation for Unit Length conversion

--- Calculator Pro/Calculator Pro/Model/Unit Converter/UnitConverterManager.swift
+++ Calculator Pro/Calculator Pro/Model/Unit Converter/UnitConverterManager.swift
@@ -15,14 +15,96 @@ struct UnitConverterManager{
         case Temperature = 2
     }
     
+    // For length conversion
     
-    enum UnitLength{
-        case millimeter, centimeter, meter, kilometer, inch, feet, yard, mile
+    enum UnitLength: Int{
+        
+        case millimeter = 0, centimeter, meter, kilometer, inch, feet, yard, mile
+        
+        func convertTo(unit to: UnitLength, value val: Double) -> Double {
+            var constant = 1.0
+            switch self {
+            case .inch:
+                if to == .centimeter {
+                    constant = 2.54
+                } else if to == .feet {
+                    constant = 0.08333333
+                } else if to == .meter {
+                    constant = 0.0254
+                } else if to == .mile {
+                    constant = 1.5783e-5
+                }
+                
+            case .centimeter:
+                if to == .inch {
+                    constant = 0.0393701
+                } else if to == .feet {
+                    constant = 0.0328084
+                } else if to == .meter {
+                    constant = 0.01
+                } else if to == .mile {
+                    constant = 6.2137e-6
+                }
+                
+            case .feet:
+                if to == .inch {
+                    constant = 12;
+                } else if to == .centimeter {
+                    constant = 30.48
+                } else if to == .meter {
+                    constant = 0.3048
+                } else if to == .mile {
+                    constant = 0.000189394
+                }
+                
+            case .meter:
+                if to == .inch {
+                    constant = 39.3701
+                } else if to == .centimeter {
+                    constant = 100
+                } else if to == .feet {
+                    constant = 3.28084
+                } else if to == .mile {
+                    constant = 0.000621371
+                }
+                
+            case .mile:
+                if to == .inch {
+                    constant = 63360
+                } else if to == .centimeter {
+                    constant = 160934
+                } else if to == .feet {
+                    constant = 5280
+                } else if to == .meter {
+                    constant = 1609.34
+                }
+            case .millimeter:
+                <#code#>
+            case .kilometer:
+                <#code#>
+            case .yard:
+                <#code#>
+            }
+            
+            return constant * val
+        }
+        
+        
+        
+        
+        
+        
+        
+        
     }
+    
+    // For mass conversion
     
     enum UnitMass{
         case milligram, gram, kilogram, ton, ounce, pound
     }
+    
+    // For temperature conversion
 
     enum UnitTemperature{
         case celsius, fahrenheit, kelvin
@@ -30,62 +112,9 @@ struct UnitConverterManager{
     
     
     
-    func setUnitFrom(_ userChoice: String){
-        
-        if unitLengthArray.contains(userChoice){
-         
-            var unitFrom: UnitLength
-          
-            if userChoice == unitLengthArray[0]{
-                unitFrom = .millimeter
-            }
-            else if userChoice == unitLengthArray[1]{
-                unitFrom = .centimeter
-            }
-            else if userChoice == unitLengthArray[2]{
-                unitFrom = .meter
-            }
-            else if userChoice == unitLengthArray[3]{
-                unitFrom = .kilometer
-            }
-            else if userChoice == unitLengthArray[4]{
-                unitFrom = .inch
-            }
-            else if userChoice == unitLengthArray[5]{
-                unitFrom = .feet
-            }
-            else if userChoice == unitLengthArray[6]{
-                unitFrom = .yard
-            }
-            else if userChoice == unitLengthArray[7]{
-                unitFrom = .mile
-            }
-            
-            
-        
-            
-        }
-        else if unitMassArray.contains(userChoice){
-            
-        }
-        else if unitTemperatureArray.contains(userChoice){
-            
-        }
-    }
+   
     
-    func setUnitTo(_ userChoice: String){
-        
-        
-    }
-    
-    
-//    func convert(with value: Double) -> Double {
-//
-//
-//
-//    }
-    
-    
+
 
     
     
