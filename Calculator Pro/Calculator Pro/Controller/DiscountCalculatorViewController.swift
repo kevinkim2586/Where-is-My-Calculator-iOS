d2e0edd1c4599e73fc133541a7f733abd6189ca6
김영채 <kevinkim2586@gmail.com>
2021-02-25 14:08:46 +0900

    Implement pressedNumber method in Discount Calculator

--- Calculator Pro/Calculator Pro/Controller/DiscountCalculatorViewController.swift
+++ Calculator Pro/Calculator Pro/Controller/DiscountCalculatorViewController.swift
@@ -3,10 +3,13 @@ import UIKit
 class DiscountCalculatorViewController: UIViewController {
     
     @IBOutlet weak var originalPriceTextField: UITextField!
-    
     @IBOutlet weak var discountPercentageTextField: UITextField!
     @IBOutlet weak var finalResultTextField: UITextField!
     
+    
+    var originalPriceWorkings: String = ""
+    var discountPercentageWorkings: String = ""
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -15,5 +18,20 @@ class DiscountCalculatorViewController: UIViewController {
     }
     
 
-   
+    @IBAction func pressedNumber(_ sender: UIButton) {
+        
+        if originalPriceTextField.isEditing{
+         
+            originalPriceWorkings += sender.currentTitle!
+            originalPriceTextField.text = originalPriceWorkings
+        }
+        else if discountPercentageTextField.isEditing{
+            
+            discountPercentageWorkings += sender.currentTitle!
+            discountPercentageTextField.text = discountPercentageWorkings
+        }
+    }
+    
+    
+    
 }
